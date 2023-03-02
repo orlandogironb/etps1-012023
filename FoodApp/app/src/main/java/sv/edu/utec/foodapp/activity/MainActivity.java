77b21cc56e83f76a528714bacbedb9cdb7797d50
orlandogironb <64709339+orlandogironb@users.noreply.github.com>
2023-03-01 19:19:18 -0600

    Foodapp

--- FoodApp/app/src/main/java/sv/edu/utec/foodapp/activity/MainActivity.java
+++ FoodApp/app/src/main/java/sv/edu/utec/foodapp/activity/MainActivity.java
@@ -6,22 +6,26 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import sv.edu.utec.foodapp.R;
 
 public class MainActivity extends AppCompatActivity {
     ConstraintLayout btnInicial;
+    EditText edtUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
        btnInicial= findViewById(R.id.btnInicio);
-
+       edtUser=findViewById(R.id.edtUser);
 
         btnInicial.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(),Principal.class));
+                Intent i=new Intent(getApplicationContext(),Principal.class);
+                i.putExtra("usuario",edtUser.getText().toString());
+                startActivity(i);
             }
         });
     }
--- FoodApp/app/src/main/java/sv/edu/utec/foodapp/activity/Principal.java
+++ FoodApp/app/src/main/java/sv/edu/utec/foodapp/activity/Principal.java
@@ -3,14 +3,20 @@ package sv.edu.utec.foodapp.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import sv.edu.utec.foodapp.R;
 
 public class Principal extends AppCompatActivity {
-
+TextView edtUserMsj;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        Bundle bundle=getIntent().getExtras();
+        edtUserMsj=findViewById(R.id.tvUsuario);
+        String inform="Hola ,"+bundle.getString("usuario");
+        edtUserMsj.setText(inform);
     }
 }
