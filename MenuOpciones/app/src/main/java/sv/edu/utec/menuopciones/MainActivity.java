3d879d6276dbfa705dd98b76e9d46d633aba7c32
orlandogironb <64709339+orlandogironb@users.noreply.github.com>
2023-03-29 20:05:25 -0600

    29032023

--- MenuOpciones/app/src/main/java/sv/edu/utec/menuopciones/MainActivity.java
+++ MenuOpciones/app/src/main/java/sv/edu/utec/menuopciones/MainActivity.java
@@ -1,6 +1,8 @@
 package sv.edu.utec.menuopciones;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,14 +11,55 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import sv.edu.utec.menuopciones.clases.CamFragment;
+import sv.edu.utec.menuopciones.clases.FavFragment;
+import sv.edu.utec.menuopciones.clases.PricipalFragment;
+import sv.edu.utec.menuopciones.clases.SearchFragment;
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView btnNav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnNav=findViewById(R.id.btnNav);
+
+        btnNav.setOnNavigationItemSelectedListener((BottomNavigationView.OnNavigationItemSelectedListener) navaListener);
     }
 
+    private BottomNavigationView.OnNavigationItemSelectedListener navaListener= new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment seleccionFrag= null;
+
+            switch (item.getItemId()){
+                case R.id.nav_home:
+                    seleccionFrag = new PricipalFragment();
+                    break;
+                case R.id.nav_fav:
+                    seleccionFrag = new FavFragment();
+                    break;
+                case R.id.nav_cam:
+                    seleccionFrag = new CamFragment();
+                    break;
+                case R.id.nav_src:
+                    seleccionFrag = new SearchFragment();
+                    break;
+        }
+
+
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentCont, seleccionFrag).commit();
+            return true;
+
+        }
+
+
+
+    };
     public boolean onCreateOptionsMenu(Menu menu)
     {
         MenuInflater inflater = getMenuInflater();
